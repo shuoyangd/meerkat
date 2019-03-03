52296600647fdc053427f77f55d60cf23623a166
Hainan Xu <hainan.xv@gmail.com>
2019-03-03 17:46:28 -0500

    fix some bugs in soft_aer.py

--- soft_aer.py
+++ soft_aer.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/python
 
 import sys
 import math
@@ -13,42 +13,61 @@ total_kl = 0.0
 total_length = 0
 
 for fline in fw:
+#  print ("line is", fline)
   fpairs = fline.split()
 
-  bline = bw.readline()
+  bline = bw.readline().replace("nan", "0.0")
+
   hyp_ali = eval(bline)
+#  print (hyp_ali)
 
   ali = eval(bline) # make a copy
+#  print (len(ali), len(ali[0]))
   for i in range(0, len(ali)):
     for j in range(0, len(ali[i])):
-      ali[i][j] = 0
+      ali[i][j] = 0.00000001
 
   m_src = {}
   m_tgt = {}
   for pairs in fpairs:
+    pairs = pairs.replace("p", "-")
+        
     a, b = pairs.split('-')
-    a = int(a)
-    b = int(b)
+    a = int(a) - 1
+    b = int(b) - 1
 
-    m_src[a] = m_src.get(a, 0) + 1
-    m_tgt[b] = m_tgt.get(b, 0) + 1
+    ali[b][a] = 1
 
-    ali[a][b] = 1
+
+  for i in range(0, len(ali)):
+    for j in range(0, len(ali[i])):
+      m_src[j] = m_src.get(j, 0) + ali[i][j]
+      m_tgt[i] = m_tgt.get(i, 0) + ali[i][j]
+
+
+  ali[len(ali) - 1][len(ali[0]) - 1] = 1 # for EOS -EOS alignment
+  m_src[len(ali[0]) - 1] = 1
+  m_tgt[len(ali) - 1] = 1
+
+#  print (ali)
+#  print (hyp_ali)
 
   kl = 0
   for i in range(0, len(ali)):
     for j in range(0, len(ali[i])):
-      ali[i][j] /= m_tgt[j]
+      ali[i][j] /= m_tgt[i]
+
 
   for i in range(0, len(ali)):
     for j in range(0, len(ali[i])):
       if ali[i][j] != 0:
-        kl += -ali[i][j] * math.log(hyp_ali[i][j])
+#        print (hyp_ali[i][j])
+        kl += -ali[i][j] * math.log(hyp_ali[i][j] + 0.0001)
 
-  print kl, len(ali[0])
+#  print kl, len(ali[0])
   total_kl += kl
-  total_length += len(ali[0])
+  total_length += len(ali[0]) + 1
 
-print "loss = ", total_kl / total_length
+print ("loss = ", total_kl / total_length)
 
 
