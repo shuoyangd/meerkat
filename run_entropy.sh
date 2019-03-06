a3c05b3a05f8d27fb5a6208e426b516c750e1eef
Hainan Xu <hainan.xv@gmail.com>
2019-03-06 15:28:28 -0500

    inviting shouyang soon

--- run_entropy.sh
+++ run_entropy.sh
@@ -9,14 +9,26 @@ export PATH="/home/hxu/anaconda3/bin:$PATH"
 mkdir -p entropies/
 dir=entropies
 
-for i in `ls -l /home/shuoyangd/projects/realign/alg_exps/out_v3/align | grep -v "\->" | awk '{print $NF}'`; do
+false && for i in `ls -l /home/shuoyangd/projects/realign/alg_exps/out_v3/align | grep -v "\->" | awk '{print $NF}'`; do
   [ ! -f /home/shuoyangd/projects/realign/alg_exps/out_v3/align/$i/out.sa ] && continue
   python load_and_entropy.py /home/shuoyangd/projects/realign/alg_exps/out_v3/align/$i/out.sa > $dir/$i.sa.txt
   python load_and_entropy.py /home/shuoyangd/projects/realign/alg_exps/out_v3/align/$i/out.at > $dir/$i.at.txt
 done
 
-for i in `ls -l /home/shuoyangd/projects/realign/alg_exps/out_v3_free/align | grep -v "\->" | awk '{print $NF}'`; do
+false && for i in `ls -l /home/shuoyangd/projects/realign/alg_exps/out_v3_free/align | grep -v "\->" | awk '{print $NF}'`; do
   [ ! -f /home/shuoyangd/projects/realign/alg_exps/out_v3_free/align/$i/out.sa ] && continue
   python load_and_entropy.py /home/shuoyangd/projects/realign/alg_exps/out_v3_free/align/$i/out.sa > $dir/$i.free.sa.txt
   python load_and_entropy.py /home/shuoyangd/projects/realign/alg_exps/out_v3_free/align/$i/out.sa > $dir/$i.free.sa.txt
+done
+
+false && for i in `ls -l /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_all_smoothfac/align | grep "\->" | awk '{print $(NF-2)}'`; do
+  [ ! -f /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_all_smoothfac/align/$i/out.sa ] && echo skip /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_all_smoothfac/align/$i && continue
+  python load_and_entropy.py /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_all_smoothfac/align/$i/out.sa > $dir/$i.all.sa.txt
+  python load_and_entropy.py /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_all_smoothfac/align/$i/out.at > $dir/$i.all.at.txt
+done
+
+for i in `ls -l /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_free_smoothfac/align | grep "\->" | awk '{print $(NF-2)}'`; do
+  [ ! -f /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_free_smoothfac/align/$i/out.sa ] && echo skip /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_free_smoothfac/align/$i && continue
+#  python load_and_entropy.py /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_free_smoothfac/align/$i/out.sa > $dir/$i.free.sa.txt
+  python load_and_entropy.py /export/c11/shuoyangd/projects/realign/alg_exps/out_v3_free_smoothfac/align/$i/out.at > $dir/$i.free.at.txt
 done
